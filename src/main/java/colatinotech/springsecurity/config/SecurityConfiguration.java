8aa36335deb28d38279dd996e2813b5457de07f9
wcolatino <wcolatino@gmail.com>
2024-04-13 10:21:23 -0300

    Melhoria nos requestMatchers permitindo e bloqueando requisições

--- src/main/java/colatinotech/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/colatinotech/springsecurity/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package colatinotech.springsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,10 +14,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity //Habilita as configurações de segurança e me dá possibilidade de fazer minhas oconfigurações;
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter filter;
 
     /*Método que configura a segurança do Spring*/
     @Bean
@@ -25,10 +30,12 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable) //desabilita o CORS
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Define uma politica sem sessão aberta;
                 .authorizeHttpRequests(
-                        auth -> auth.requestMatchers(HttpMethod.GET, "/usuarios").hasRole("ADMIN") //Toda requisição em usuários tem de ter a ROLE ADMIN.
-                                .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+                        auth -> auth.requestMatchers(HttpMethod.GET, "/usuarios/admin").hasRole("ADMIN") //Toda requisição em usuários tem de ter a ROLE ADMIN.
+                                .requestMatchers(HttpMethod.POST, "/usuarios/user").hasRole("USER")
                                 .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/h2-console").permitAll()
                                 .anyRequest().authenticated()) //Qualquer requisição o usuário tem de estar authenticado
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
